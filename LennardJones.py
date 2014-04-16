00973b516852c0b35bdc4abf3625c1f8e8bc9458
Hendrik Kaju <hendrik.kaju@chem.ox.ac.uk>
2014-04-16 16:43:19 +0100

    Add algorithm outlines

--- LennardJones.py
+++ LennardJones.py
@@ -1,7 +1,21 @@
+from fluid import LJContainer
+
+NUM_PARTICLES = 108
+TIME_STEP = 0.001
+
 class LennardJones:
 
-    def __init__(self):
-        pass
+    def __init__(self, density, temperature):
+        #Initialize the container
+        container = LJContainer()
+        #Equilibriate the system
+        #Start measuring
+        while self.t < run_length:
+            #Calculate the forces
+            #Integrate equations of motion
+            t += TIME_STEP
+            #Sample averages
+        #Generate a plot of the energies (kinetic, potential, total)
 
 if __name__ == "__main__":
     LennardJones()
--- fluid.py
+++ fluid.py
@@ -1,4 +1,55 @@
-class LJFluid:
+class Particle:
 
-    def __init__(self):
+    #Particle mass
+    _m = 1
+
+    #Position
+    x = 0
+    y = 0
+    z = 0
+
+    #Velocity
+    vx = 0
+    vy = 0
+    vz = 0
+
+    #Force
+    fx = 0
+    fy = 0
+    fz = 0
+
+    def __init__(self, position, velocity):
+
+        #Set initial position and velocity from provided tuples
+        self.x, self.y, self.z = position
+        self.vx, self.vy, self.vz = velocity
+
+    def set_force_to(self, force):
+        self.fx, self.fy, self.fz = force
+
+    def update_position(self, dt):
+        pass
+
+class LJContainer:
+
+    _particles = []
+
+    def __init__(self, density, tempoerature):
+        pass
+
+    def initialize(self, atoms):
+        #Place particles
+        #Give random velocities
+        #Update CoM velocity
+        #Update total kinetic energy
+        #Shift velocities so that CoM velocity is zero
+        #Scale velocities according to the equipartition theorem
+        pass
+
+    def update_forces(self):
+        #Loop over all pairs
+        #Use PBC
+        #Test cutoff
+        #Update force according to LJ
+        #Update energy
         pass
