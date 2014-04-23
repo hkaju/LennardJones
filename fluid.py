1ee13be4dad91bf36240b25f3e94cdd0470d6265
Hendrik Kaju <hendrik.kaju@chem.ox.ac.uk>
2014-04-23 17:06:55 +0100

    Implement initial velocity generation

--- fluid.py
+++ fluid.py
@@ -1,4 +1,6 @@
 import copy
+import random
+import math
 
 class Particle:
 
@@ -42,18 +44,73 @@ class Particle:
 class LJContainer:
 
     _particles = []
+    _temperature = 0
+    _density = 0
+    _length = 0
 
-    def __init__(self, density, temperature):
-        pass
+    def __init__(self, number_of_particles, density, temperature):
+        self._length = (number_of_particles / density)**(1.0/3)
+        self._temperature = temperature
+        self._density = density
+        #self.initialize(number_of_particles)
+        self.generate_velocities(108)
+        #print(self._length)
 
-    def initialize(self, atoms):
-        #Place particles
-        #Give random velocities
-        #Update CoM velocity
-        #Update total kinetic energy
-        #Shift velocities so that CoM velocity is zero
-        #Scale velocities according to the equipartition theorem
-        pass
+    def initialize(self, number_of_particles):
+        """Initialize the container."""
+
+        #Calculate the spacing between particles
+        spacing = self._length / 6.0
+        velocities = self.generate_velocities(number_of_particles)
+        for n in range(number_of_particles):
+            #Start with 3 layers of 6x6 particles
+            x = (n%6 + 1) * spacing
+            y = ((n-(n/36)*36)/6 + 1) * spacing
+            z = (n/36 + 1) * spacing
+            position = (x, y, z,)
+            velocity = velocities.pop()
+            particle = Particle(position, velocity)
+            self._particles.append(particle)
+
+    def generate_velocities(self, number_of_particles):
+        """Generate an initial velocity distribution of number_of_particles velocities."""
+
+        dimensions = 3
+
+        velocities = []
+        vtot = [0] * dimensions
+        v2tot = [0] * dimensions
+        fs = [0] * dimensions
+
+        for i in range(number_of_particles):
+            v = [0] * dimensions
+            v2 = [0] * dimensions
+            for d in range(dimensions):
+                vn = random.uniform(-0.5, 0.5)
+                v[d] = vn
+                v2[d] = vn**2
+                vtot[d] += vn
+                v2tot[d] += vn**2
+            velocities.append(v)
+        for d in range(dimensions):
+            vtot[d] = vtot[d] / number_of_particles
+            v2tot[d] = v2tot[d] / number_of_particles
+            fs[d] = math.sqrt(self._temperature / v2tot[d])
+        for i in range(number_of_particles):
+            vn = [0] * dimensions
+            for d in range(dimensions):
+                vn[d] = (velocities[i][d] - vtot[d]) * fs[d]
+            velocities[i] = vn
+
+        vtot = [0] * dimensions
+        v2tot = [0] * dimensions
+        for item in velocities:
+            for dim in range(len(item)):
+                vtot[dim] += item[dim]
+                v2tot[dim] += item[dim]**2
+        for dim in range(dimensions):
+            v2tot[dim] = v2tot[dim] / number_of_particles
+        print vtot, v2tot
 
     def update_forces(self):
         #Loop over all pairs
