ccf2b687d1243885756f72daa802f9ede4e292dc
Hendrik Kaju <hendrik.kaju@chem.ox.ac.uk>
2014-04-26 22:46:03 +0100

    First working version of the model

--- LennardJones.py
+++ LennardJones.py
@@ -2,26 +2,37 @@
 
 from fluid import LJContainer
 
-PARTICLES = 108.0
-TEMPERATURE = 2.0
-DENSITY = 1.0
+PARTICLES = 108
+TEMPERATURE = 0.728
+DENSITY = 0.8442
 TIME_STEP = 0.001
-STEPS = 2000
+STEPS = 20
 
 class LennardJones:
 
-    _t = 0
+    t = 0
 
     def __init__(self):
         #Initialize the container
         container = LJContainer(PARTICLES, DENSITY, TEMPERATURE)
+        #print container.generate_velocities(2)
         #Equilibriate the system
         #Start measuring
-        while self._t < STEPS:
-            #Calculate the forces
-            #Integrate equations of motion
-            self._t += TIME_STEP
+        while self.t < STEPS:
+            container.reset_forces()
+            container.update_forces()
+            container.tick(TIME_STEP)
+            container.sample(self.t)
+            self.t += TIME_STEP
             #Sample averages
+        #print container.data
+        container.write_particles()
+        f = open("output.csv", "w")
+        f.write("t,K,V,T\n")
+        for item in range(len(container.data["t"])):
+            f.write("{0},{1},{2},{3}\n".format(container.data["t"][item], container.data["K"][item],
+                container.data["V"][item], container.data["T"][item]))
+        f.close()
         #Generate a plot of the energies (kinetic, potential, total)
 
 if __name__ == "__main__":
--- fluid.py
+++ fluid.py
@@ -2,75 +2,98 @@ import copy
 import random
 import math
 
+DEBUG = True
+
 class Particle:
 
     #Particle properties
-    _position = (0, 0, 0,)
-    _previous_position = (0, 0, 0,)
-    _velocity = (0, 0, 0,)
-    _force = (0, 0, 0,)
+    position = [0, 0, 0]
+    previous_position = None
+    velocity = [0, 0, 0]
+    force = [0, 0, 0]
+    potential = 0
 
     def __init__(self, position, velocity):
         """Initialize the particle with given position and velocity."""
 
-        self._position = position
-        self._velocity = velocity
-
-    def set_force_to(self, force):
-        self._force = force
+        self.position = position
+        self.velocity = velocity
 
     def update_position(self, dt):
         """Update the position of the particle according to the Verlet algorithm."""
 
+        #If this is the first time the particle moves, generate a previous position
+        #based on current speed
+        if not self.previous_position:
+            x = self.position[0] - self.velocity[0]*dt
+            y = self.position[1] - self.velocity[1]*dt
+            z = self.position[2] - self.velocity[2]*dt
+            self.previous_position = [x, y, z]
+
+        #Calculate updated position and velocity
         next_position = []
         next_velocity = []
-        for n in len(self._position):
-            dq = 2*self._position[n] - self._previous_position[n] + self._force[n]*dt**2
-            dv = (dq - self._previous_position[n])/(2*dt)
+        for n in range(len(self.position)):
+            dq = 2*self.position[n] - self.previous_position[n] + self.force[n]*dt**2
+            dv = (dq - self.previous_position[n])/(2*dt)
             next_position.append(dq)
             next_velocity.append(dv)
-        self._previous_position = copy.deepcopy(self._position)
-        self._position = tuple(next_position)
-        self._velocity = tuple(next_velocity)
+
+        #Update particle properties
+        self.previous_position = copy.deepcopy(self.position)
+        self.position = next_position
+        self.velocity = next_velocity
 
     def get_squared_velocity(self):
         """Get the square of the velocity."""
 
         sumv2 = 0
-        for n in len(self._velocity):
-            sumv += self._velocity[n]**2
+        for n in range(len(self.velocity)):
+            sumv2 += self.velocity[n]**2
         return sumv2
 
 class LJContainer:
 
-    _particles = []
-    _temperature = 0
-    _density = 0
-    _length = 0
+    particles = []
+    temperature = 0
+    density = 0
+    length = 0
+    data = {"t" : [],
+            "K" : [],
+            "V" : [],
+            "T" : []}
 
     def __init__(self, number_of_particles, density, temperature):
-        self._length = (number_of_particles / density)**(1.0/3)
-        self._temperature = temperature
-        self._density = density
-        #self.initialize(number_of_particles)
-        self.generate_velocities(108)
-        #print(self._length)
+        self.length = (number_of_particles * 4.0/3.0 * math.pi / density)**(1.0/3)
+        self.temperature = temperature
+        self.density = density
+        self.initialize(number_of_particles)
+        #self.write_particles()
+
+    def write_particles(self):
+        f = open("grid.csv", "w")
+        f.write("x,y,z\n")
+        for particle in self.particles:
+            f.write("{0},{1},{2}\n".format(particle.position[0], particle.position[1], particle.position[2]))
+        f.close()
 
     def initialize(self, number_of_particles):
         """Initialize the container."""
 
         #Calculate the spacing between particles
-        spacing = self._length / 6.0
+        spacing = self.length / 6.0
         velocities = self.generate_velocities(number_of_particles)
+
         for n in range(number_of_particles):
             #Start with 3 layers of 6x6 particles
             x = (n%6 + 1) * spacing
             y = ((n-(n/36)*36)/6 + 1) * spacing
             z = (n/36 + 1) * spacing
+
             position = (x, y, z,)
             velocity = velocities.pop()
             particle = Particle(position, velocity)
-            self._particles.append(particle)
+            self.particles.append(particle)
 
     def generate_velocities(self, number_of_particles):
         """Generate an initial velocity distribution of number_of_particles velocities."""
@@ -95,27 +118,67 @@ class LJContainer:
         for d in range(dimensions):
             vtot[d] = vtot[d] / number_of_particles
             v2tot[d] = v2tot[d] / number_of_particles
-            fs[d] = math.sqrt(self._temperature / v2tot[d])
+            fs[d] = math.sqrt(self.temperature / v2tot[d])
         for i in range(number_of_particles):
             vn = [0] * dimensions
             for d in range(dimensions):
                 vn[d] = (velocities[i][d] - vtot[d]) * fs[d]
             velocities[i] = vn
 
-        vtot = [0] * dimensions
-        v2tot = [0] * dimensions
-        for item in velocities:
-            for dim in range(len(item)):
-                vtot[dim] += item[dim]
-                v2tot[dim] += item[dim]**2
-        for dim in range(dimensions):
-            v2tot[dim] = v2tot[dim] / number_of_particles
-        print vtot, v2tot
+        if DEBUG:
+            vtot = [0] * dimensions
+            v2tot = [0] * dimensions
+            for item in velocities:
+                for dim in range(len(item)):
+                    vtot[dim] += item[dim]
+                    v2tot[dim] += item[dim]**2
+            for dim in range(dimensions):
+                v2tot[dim] = v2tot[dim] / number_of_particles
+            print vtot, v2tot
+
+        return velocities
 
     def update_forces(self):
-        #Loop over all pairs
-        #Use PBC
-        #Test cutoff
-        #Update force according to LJ
-        #Update energy
-        pass
+        rc2 = 2.5**2
+        ecut = 4 * ((1/2.5)**12 - (1/2.5)**6)
+        for i in range(0, len(self.particles) - 1):
+            for j in range(i, len(self.particles)):
+                if i != j:
+                    dn = [self.particles[i].position[0] - self.particles[j].position[0],
+                          self.particles[i].position[1] - self.particles[j].position[1],
+                          self.particles[i].position[2] - self.particles[j].position[2]]
+                    dn[0] = dn[0] - self.length*round(dn[0]/self.length)
+                    dn[1] = dn[1] - self.length*round(dn[1]/self.length)
+                    dn[2] = dn[2] - self.length*round(dn[2]/self.length)
+                    r2 = dn[0]**2 + dn[1]**2 + dn[2]**2
+                    if r2 < rc2:
+                        r2i = 1/r2
+                        r6i = r2i**3
+                        u = 4 * r6i * (r6i - 1)  - ecut
+                        f = 48 * r2i * r6i * (r6i - 0.5)
+                        self.particles[i].potential += u
+                        self.particles[j].potential += u
+                        for d in range(3):
+                            self.particles[i].force[d] += f*dn[d]
+                            self.particles[j].force[d] -= f*dn[d]
+
+    def tick(self, time_step):
+        for p in self.particles:
+            p.update_position(time_step)
+
+    def reset_forces(self):
+        for i in range(len(self.particles)):
+            force = [0] * 3
+            self.particles[i].force = force
+            self.particles[i].potential = 0
+
+    def sample(self, time):
+        self.data["t"].append(time)
+        K = 0
+        V = 0
+        for particle in self.particles:
+            K += particle.get_squared_velocity()
+            V += particle.potential
+        self.data["K"].append(K/len(self.particles))
+        self.data["V"].append(V/len(self.particles))
+        self.data["T"].append((K + V)/len(self.particles))
