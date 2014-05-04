92f9e98e34718c7ba7540dcba5fb2cac88851e4b
Hendrik Kaju <hendrik.kaju@chem.ox.ac.uk>
2014-05-04 22:36:55 +0100

    Clean up LennardJones.py

--- LennardJones.py
+++ LennardJones.py
@@ -6,30 +6,44 @@ from fluid import LJContainer
 import util
 
 PARTICLES = 108
-TEMPERATURE = 0.728
+TEMPERATURE = 2.0
 DENSITY = 0.8442
-TIME_STEP = 0.005
-STEPS = 3000
+TIME_STEP = 0.004
+STEPS = 10000
 
 class LennardJones:
 
     t = 0.0
 
     def __init__(self, timestep=TIME_STEP):
+
         #Initialize the container
         container = LJContainer(PARTICLES, DENSITY, TEMPERATURE)
+
+        #Generate forces for the initial configuration
+        #container.update_forces()
+
         #Equilibriate the system
+        #container.equilibriate()
+
         #Start measuring
         while self.t < STEPS:
             sys.stdout.write("\rCalculating: {0:3.1f}%".format(self.t*100/STEPS))
             sys.stdout.flush()
+
+            #Do one 'tick' consisting of two integrations and a force update inbetween
             container.tick(timestep)
-            container.sample(self.t)
-            self.t += 1
+
             #Sample averages
+            container.sample(self.t)
+
+            self.t += 1
+
+        #Store sampling data
         util.write_data(container.data)
-        util.generate_report(timestep)
+
         #Generate a plot of the energies (kinetic, potential, total)
+        util.generate_report(timestep)
 
 if __name__ == "__main__":
     lj = None
