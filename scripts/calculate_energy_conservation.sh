610cad446b5f398b8113b2a0aa0a1bab87e09e35
Hendrik Kaju <hendrik.kaju@chem.ox.ac.uk>
2014-05-04 22:21:52 +0100

    Update energy conservation data generation script

--- scripts/calculate_energy_conservation.sh
+++ scripts/calculate_energy_conservation.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
-./LennardJones.py dt0.1
-./LennardJones.py dt0.5
-./LennardJones.py dt0.05
-./LennardJones.py dt0.01
-./LennardJones.py dt0.005
 ./LennardJones.py dt0.001
+./LennardJones.py dt0.003
+./LennardJones.py dt0.006
+./LennardJones.py dt0.01
+./LennardJones.py dt0.02
+./LennardJones.py dt0.03
+./LennardJones.py dt0.04
+./LennardJones.py dt0.05
+./LennardJones.py dt0.06
+./LennardJones.py dt0.07
+./LennardJones.py dt0.08
+./LennardJones.py dt0.09
+./LennardJones.py dt0.1
