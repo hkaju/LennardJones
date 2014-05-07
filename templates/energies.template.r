c77c8a6369846464a6eb5c569dbe595f6e8fe108
Hendrik Kaju <hendrik.kaju@chem.ox.ac.uk>
2014-05-07 03:32:17 +0100

    Update templates

--- templates/energies.template.r
+++ templates/energies.template.r
@@ -1,16 +1,19 @@
-pdf("reports/energies-dt{timestep}.pdf")
+pdf("reports/results-dn{density}.pdf")
 
-data <- read.csv("output.csv", header=T)
+data <- read.csv("data/data-dn{density}.csv", header=T)
 
-plot(data$t, data$T, ylim=c(-6, 6), xlab="Steps", ylab="Energy per particle", type="n", main=expression(paste("Step size ", delta, "t = {timestep}")))
+plot(data$t, data$T, ylim=c(-10, 10), xlab="Steps", ylab="Energy per particle", type="n", main=expression(paste("Step size ", delta, "t = {density}")))
 lines(data$t, data$V, col="blue")
 lines(data$t, data$T, col="black")
 lines(data$t, data$K, col="red")
 
-legend("right", cex=0.75, pch=NA, lty=c(1, 1, 1),
+legend("topright", cex=0.75, pch=NA, lty=c(1, 1, 1),
   col=c("red", "black", "blue"), legend=c("Kinetic", "Total", "Potential"))
 
-plot(data$t, data$temp, xlab="Steps", ylab="Temperature", type="n", main=expression(paste("Step size ", delta, "t = {timestep}")))
+plot(data$t, data$temp, xlab="Steps", ylab="Temperature", type="n", main=expression(paste("Temperature at ", rho, " = {density}")))
 lines(data$t, data$temp, col="black")
+
+plot(data$t, data$P, xlab="Steps", ylab="Pressure", type="n", main=expression(paste("Pressure at ", rho, " = {density}")))
+lines(data$t, data$P, col="black")
 
 dev.off()
